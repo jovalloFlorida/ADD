3ee5498b996fb47e666f74e74420ec6bff88c3d9
Jose Valverde <jovall@floridauniversitaria.es>
2022-01-12 20:11:14 +0100

    Mejora Grafica

--- AE06_MVC/src/es/florida/AE06_MVC/Vista.java
+++ AE06_MVC/src/es/florida/AE06_MVC/Vista.java
@@ -28,7 +28,7 @@ public class Vista {
 
 	public void inicialize() {
 		frame = new JFrame();
-		frame.setBounds(300, 300, 950, 425);
+		frame.setBounds(300, 300, 891, 499);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setResizable(false);
@@ -36,7 +36,7 @@ public class Vista {
 		frame.setTitle("Gestion Biblioteca. AE06 MongoDB");
 
 		JScrollPane scrollPane_Original = new JScrollPane();
-		scrollPane_Original.setBounds(10, 10, 920, 230);
+		scrollPane_Original.setBounds(10, 10, 688, 426);
 		frame.getContentPane().add(scrollPane_Original);
 
 		textArea_Texto = new JTextArea();
@@ -49,32 +49,32 @@ public class Vista {
 		
 		btnMostrarBiblioteca = new JButton("Mostrar Biblioteca");
 		btnMostrarBiblioteca.setFont(new Font("Arial", Font.PLAIN, 12));
-		btnMostrarBiblioteca.setBounds(20, 250, 120, 27);
+		btnMostrarBiblioteca.setBounds(720, 65, 130, 28);
 		frame.getContentPane().add(btnMostrarBiblioteca);
 
 		btnMostrar = new JButton("Mostrar Libro");
 		btnMostrar.setFont(new Font("Arial", Font.PLAIN, 12));
-		btnMostrar.setBounds(20, 287, 120, 27);
+		btnMostrar.setBounds(720, 102, 130, 28);
 		frame.getContentPane().add(btnMostrar);
 		
 		btnCrear = new JButton("Crear Libro");
 		btnCrear.setFont(new Font("Arial", Font.PLAIN, 12));
-		btnCrear.setBounds(20, 324, 120, 27);
+		btnCrear.setBounds(720, 139, 130, 28);
 		frame.getContentPane().add(btnCrear);
 		
 		btnActualizar = new JButton("Actualizar Libro");
 		btnActualizar.setFont(new Font("Arial", Font.PLAIN, 12));
-		btnActualizar.setBounds(160, 287, 120, 27);
+		btnActualizar.setBounds(720, 176, 130, 28);
 		frame.getContentPane().add(btnActualizar);
 		
 		btnBorrar = new JButton("Borrar libro");
 		btnBorrar.setFont(new Font("Arial", Font.PLAIN, 12));
-		btnBorrar.setBounds(160, 324, 120, 27);
+		btnBorrar.setBounds(720, 213, 130, 28);
 		frame.getContentPane().add(btnBorrar);
 		
 		btnSalir = new JButton("Salir");
 		btnSalir.setFont(new Font("Arial", Font.PLAIN, 12));
-		btnSalir.setBounds(779, 339, 120, 27);
+		btnSalir.setBounds(730, 395, 120, 27);
 		frame.getContentPane().add(btnSalir);
 
 		this.frame.setVisible(true);
